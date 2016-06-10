ff70500f3552256a1d00dd171f0a04349a8bb75d
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-06-11 00:55:04 +0200

    Test editing todo lists

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+describe "Editing todo lists" do
+
+  let(:todo_list) {TodoList.create(title: 'Groceries', description: 'Grocery list.')}
+
+  def update_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list."
+
+    todo_list = options[:todo_list]
+
+    visit '/todo_lists'
+
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Edit"
+    end
+
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
+    click_button "Update Todo list"
+
+  end
+  it 'updates a todo list succesfully with correct information' do
+
+
+    update_todo_list  todo_list: todo_list,
+                      title: "New title",
+                      description: "New description"
+
+    todo_list.reload
+
+    expect(page).to have_content("Todo list was successfully updated")
+    expect(todo_list.title).to eq("New title")
+    expect(todo_list.description).to eq("New description")
+  end
+
+  it "displays an error when the todo list has no title" do
+
+    update_todo_list  todo_list: todo_list,
+                      title: ''
+
+    expect(page).to have_content("error")
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+
+  it "displays an error when the todo list has a title less than 3 characters" do
+
+    update_todo_list  todo_list: todo_list,
+                      title: 'Hi'
+
+    expect(page).to have_content("error")
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+  it "displays an error when the todo list has no description" do
+
+    update_todo_list  todo_list: todo_list,
+                      description: ''
+
+    expect(page).to have_content("error")
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+  it "displays an error when the todo list has a description less than 5 characters" do
+
+
+    update_todo_list  todo_list: todo_list,
+                      description: "this"
+
+    expect(page).to have_content("error")
+
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today.")
+  end
+
+end
