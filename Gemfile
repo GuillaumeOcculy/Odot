64b5f7260840f8f90e8cb67df38865bf88bc91e0
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-06-11 01:29:27 +0200

    Add todo item modal ans associated specs

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ end
 
 group :test do
     gem 'capybara'
+    gem 'shoulda-matchers', '~> 3.1'
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,11 @@
 class TodoList < ActiveRecord::Base
 
+  # Associations
+  has_many :todo_items
+  
+  # Validation
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }
+
+
 end
