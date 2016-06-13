04d5defbfcce20b5d50757f7e3f8d35a0bed8463
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-06-13 02:41:16 +0200

    Refactor views

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -6,6 +6,10 @@ class TodoItem < ActiveRecord::Base
   # Validations
   validates :content, presence: true, length: { minimum: 2 }
 
+  # Scopes
+  scope :complete, -> { where("completed_at is not null") }
+  scope :incomplete, -> { where(completed_at: nil) }
+  
   def completed?
     !completed_at.blank?
   end
